ec754c2a507afc60548eae34b38726b24a5b12f9
Mingen Pan <panmingen@uchicago.edu>
2020-02-22 22:46:58 -0500

    pass cucumber

--- app.rb
+++ app.rb
@@ -40,6 +40,23 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      valid = @game.guess letter
+    rescue ArgumentError
+      flash[:message] = "Invalid guess!"
+      redirect '/show'
+      break
+    end
+
+    status = @game.check_win_or_lose
+    if status == :win
+      redirect '/win'
+    elsif status == :lose
+      redirect '/lose'
+    end
+    if valid == false
+      flash[:message] = "You have already used that letter"
+    end
     redirect '/show'
   end
   
